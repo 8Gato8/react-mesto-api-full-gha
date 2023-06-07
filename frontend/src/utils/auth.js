46d6ba2f05e4ec614e13e2bb84094db83cee7efc
VM 8Gato8 <Gato334@yandex.ru>
2023-06-07 10:14:12 +0000

    Исправить опечатку в makeRequest функции

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,7 +1,7 @@
 import { makeRequest } from "./makeRequest";
 
 export function register(password, email) {
-	return makeRequest('signup', 'POST', { password, email });
+	return makeRequest('/signup', 'POST', { password, email });
 }
 
 export function authorize(password, email) {
